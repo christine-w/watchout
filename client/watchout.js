654bf49f9cd7656cf2e20d7094409d925a720e80
christine-w <christine.wong@gmail.com>
2017-09-05 13:50:08 -0700

    Enable user to drag player dot around

--- client/watchout.js
+++ client/watchout.js
@@ -6,9 +6,8 @@ class ColliderGame {
   }
 
   init() {
-    const player = document.getElementById('player');
-
     this.drawEnemies();
+    this.drawPlayer();
   }
 
   drawEnemies() {
@@ -29,6 +28,28 @@ class ColliderGame {
     setTimeout(this.drawEnemies.bind(this), 1000);
   }
 
+  drawPlayer() {
+    d3.select('#gameBoard').selectAll('#player').data([[25, 25]]).enter()
+      .append('circle')
+      .attr('id', 'player')
+      .attr('r', 1)
+      .attr('fill', 'blue')
+      .attr('cx', 25)
+      .attr('cy', 25)
+      .attr("transform", "translate(" + 0 + "," + 0 + ")")
+      .call(d3.behavior.drag().on('drag', this.dragPlayer));
+  }
+
+  dragPlayer(d) {
+      console.log('DRAG!');
+        d3.select(this)
+        .attr('cx', d.x = d3.event.x)
+        .attr('cy', d.y = d3.event.y);
+  }
 }
 
 const game = new ColliderGame();
+
+function dragPlayer(event) {
+  console.log('drag detected');
+};
