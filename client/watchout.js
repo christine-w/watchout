ae9b313cf385c64109b77ccca61bf5d563d191d4
christine-w <christine.wong@gmail.com>
2017-09-05 12:51:34 -0700

    Redraw enemies in random new positions each second Also start with player in the center of the board.

--- client/watchout.js
+++ client/watchout.js
@@ -2,25 +2,33 @@
 class ColliderGame {
   constructor() {
     this.NUM_ENEMIES = 20;
-    this.enemyPositions = [];
     this.init();
-    //document.getElementsByClassName('highscore')[0].classList.add('test');
   }
 
   init() {
+    const player = document.getElementById('player');
+
     this.drawEnemies();
   }
 
   drawEnemies() {
+    const enemyPositions = [];
+
+    d3.select('#gameBoard').selectAll('.enemy').data(enemyPositions).exit().remove();
+
     for (let i = 0; i < this.NUM_ENEMIES; i++) {
-      this.enemyPositions[i] = [Math.floor(Math.random()*50), Math.floor(Math.random()*50)];
+      enemyPositions[i] = [Math.floor(Math.random()*50), Math.floor(Math.random()*50)];
     }
-    d3.select('svg').selectAll('circle').data(this.enemyPositions).enter()
+    d3.select('#gameBoard').selectAll('.enemy').data(enemyPositions).enter()
       .append('circle')
+      .attr('class', (d) => 'enemy')
       .attr('r', (d) => 1)
       .attr('cx', (d) => d[0])
       .attr('cy', (d) => d[1]);
+
+    setTimeout(this.drawEnemies.bind(this), 1000);
   }
+
 }
 
 const game = new ColliderGame();
